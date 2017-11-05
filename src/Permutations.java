9bd398a67d48853c9ee6f660cd404ae4de88e31e
Leo_Zinger <leo.zinger@gmail.com>
2017-11-05 08:25:15 -0800

    putting lists as a parameter.

--- src/Permutations.java
+++ src/Permutations.java
@@ -3,18 +3,20 @@ import java.util.ArrayList;
 //yet another elegant recursive solution
 public class Permutations {
     public static void main( String[] args ) {
-        ArrayList<ArrayList<Integer>> lists = new ArrayList<ArrayList<Integer>>();
-        permu(0, "ABCD");
+        ArrayList<String> lists = new ArrayList<String>();
+        permu(0, "ABCD", lists);
+        System.out.println(lists);
     }
-    static void permu(int fixed, String s) {
+    static void permu(int fixed, String s, ArrayList<String> lists) {
         char[] chr = s.toCharArray();
         if (fixed == s.length())
-            System.out.println(s);
+//            System.out.println(s);
+            lists.add(s);
         for (int i = fixed; i < s.length(); i++) {
             char c = chr[i];
             chr[i] = chr[fixed];
             chr[fixed] = c;
-            permu(fixed + 1, new String(chr));
+            permu(fixed + 1, new String(chr), lists);
         }
     }
 }
